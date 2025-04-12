06e1f47d520cdd7fadda4f167e8937a337b5a9cc
sonipriyansh17 <sonipriyansh2003@gmail.com>
2025-04-13 00:47:34 +0530

    deleted index

--- realtime.py
+++ realtime.py
@@ -1,15 +1,15 @@
 import cv2
-import numpy as np 
+import numpy as np
 import os
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
-from tensorflow.python.keras.models import model_from_json  
+from tensorflow.python.keras.models import model_from_json
 
 json_file = open("facialemotionmodel.json", "r")
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
 
-model.load_weights("facialemotionmodel.h5")  
+model.load_weights("facialemotionmodel.h5")
 
 haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(haar_file)
@@ -17,7 +17,7 @@ face_cascade = cv2.CascadeClassifier(haar_file)
 def extract_feature(image):
     feature = np.array(image)
     feature = feature.reshape(1, 48, 48, 1)
-    return feature / 255.0 
+    return feature / 255.0
 
 webcam = cv2.VideoCapture(0)
 labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
@@ -26,13 +26,13 @@ while True:
     ret, frame = webcam.read()
     if not ret:
         break
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
 
         face = gray[y:y + h, x:x + w]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         
         face = cv2.resize(face, (48, 48))
